2405f54f1a3cd889c926c64def22c849c3457834
lwhdk <34403162+lwhdk@users.noreply.github.com>
2019-03-28 21:23:24 +0100

    Update guesanNumber.ps1

--- guesanNumber.ps1
+++ guesanNumber.ps1
@@ -2,6 +2,8 @@
 ####  F.eks en hængeås med blot 4 tal herunder
 ### https://medieserver.jemogfix.dk/api/v1/products/GetPrimaryProductImage?productGroupNumber=5106&productNumber=9037122&shopLanguage=1&previewSize=700
 
+
+
 $input = Read-Host -Prompt "Skriv et tal mellem 0 og 999999"
 cls
 $i = 0
@@ -9,16 +11,19 @@ $starttid = (get-date).Millisecond
 
 while($true)
 {
-$i +=1
+
 
 if ($i -eq $input)
 {
     Write-host "det indskrevende tal var: " $i
     $sluttid = (get-date).Millisecond
-## tidsberegning skal ligges efter    
-    Write-host " Det tog "  (NEW-TIMESPAN –End $sluttid -start $starttid).TotalMilliseconds  " millisekunder at gætte koden "
+    
+    Write-host " Det tog "  (NEW-TIMESPAN -start $starttid  –End $sluttid).TotalMilliseconds  " millisekunder at gætte koden "
     break
+    
 }
+write-host $i
+$i +=1
 }
 
 
